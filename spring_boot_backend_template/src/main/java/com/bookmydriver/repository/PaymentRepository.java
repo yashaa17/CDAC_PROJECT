e39dec1ffe2772890fe407790866714ee776f026
Yash Sadabal <166600732+yashaa17@users.noreply.github.com>
2024-08-22 12:21:27 +0530

    Delete spring_boot_backend_template directory

--- spring_boot_backend_template/src/main/java/com/bookmydriver/repository/PaymentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bookmydriver.repository;
-
-import com.bookmydriver.entity.Payment;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PaymentRepository extends JpaRepository<Payment, Long> {
-}
